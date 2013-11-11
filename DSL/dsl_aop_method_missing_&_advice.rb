e2b14c896d9375b8faa01a852e0767f5e35a2829
overflowDT <fedecasares1993@yahoo.com.ar>
2013-11-11 14:42:49 -0300

    Agregado del methodmissing q se habia quedado colgado

---
+++ DSL/dsl_aop_method_missing_&_advice.rb
@@ -0,0 +1,20 @@
+module Dsl_Aop
+  refine Object do
+    def method_missing(symbol, *args, &bloque)
+      met=symbol.to_s
+      if (met.start_with?('class','method'))
+        @dsl_pc_builder.send(symbol,*args)
+      else
+        if ([:after,:before,:on_error,:instead].include?(symbol))
+          getAspect.add_behaviour(symbol,bloque)
+        else
+          super
+        end
+      end
+    end
+    def advices(&bloque)
+      bloque.call
+    end
+  end
+
+end
